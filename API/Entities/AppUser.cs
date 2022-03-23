13093a17c783c84f540aa1bf2946628839adac14
DougBellinger <doug.bellinger@gmail.com>
2022-03-23 15:11:37 -0400

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,19 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public String UserName { get; set;}
-        private int myVar;
-        public int MyProperty 
-        {
-            get {return myVar; }
-            set { myVar = value;}
-        }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
